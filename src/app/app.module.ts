e80dcc29c16ed34c695c680697e1ac2652611482
foufa102022 <chetouiiftikhar@gmail.com>
2023-01-05 17:40:56 +0100

    version1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import { AppareilItemComponent } from './appareil-item/appareil-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactComponent,
+    AppareilComponent,
+    AppareilItemComponent
   ],
   imports: [
     BrowserModule,
